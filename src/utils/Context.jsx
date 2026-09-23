0ee63ddcf62fe00988b86b18e9bdc4b981a31748
Adil <adilakhtar182@gmail.com>
2026-09-23 19:56:30 +0530

    Fix empty product collection

--- src/utils/Context.jsx
+++ src/utils/Context.jsx
@@ -1,11 +1,12 @@
+import React, { createContext, useState } from "react";
 
-import React, { createContext, useEffect, useState } from "react";
 export const ProductContext = createContext();
 
 function Context(props) {
   const [products, setProducts] = useState(
-    JSON.parse(localStorage.getItem("products")) || null,
+    JSON.parse(localStorage.getItem("products")) || [],
   );
+
   return (
     <ProductContext.Provider value={[products, setProducts]}>
       {props.children}
